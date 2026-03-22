5bdab4548a735e2d7ed3add973637753101e1ee2
tttatata <85152122+tttatata@users.noreply.github.com>
2026-03-22 15:38:38 +0700

    Thêm ádasdbackend vgggg

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,6 @@ const connectDB = require("../src/config/db");
 // Mở kết nối Database mỗi khi có request tới Serverless API
 connectDB();
 
+console.log("🚀 Serverless Backend đang hoạt động trên Vercel!");
 // Export app Express để Vercel tự động nhận diện
 module.exports = app;
--- index.js
+++ index.js
@@ -3,4 +3,6 @@ require("dotenv").config();
 // Đường dẫn này trỏ tới file chạy server của bạn (giả sử bạn để trong thư mục backend/server.js)
 const app = require("../backend/server"); 
 
+console.log("🚀 Backend API đã chạy và sẵn sàng nhận request!");
+
 module.exports = app;
