74f46c5a9153b1dfe56965c34cb8fdca0d9ad345
tttatata <85152122+tttatata@users.noreply.github.com>
2026-03-22 15:34:48 +0700

    Thêm ádasdbackend vgggg

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const app = require("../src/app");
+const app = require("../src/App");
 const connectDB = require("../src/config/db");
 
 // Mở kết nối Database mỗi khi có request tới Serverless API
