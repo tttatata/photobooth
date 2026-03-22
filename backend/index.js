6b9d0b6b658694d8676e0c9510ee179789d189e8
tttatata <85152122+tttatata@users.noreply.github.com>
2026-03-22 15:48:44 +0700

    Thêm ádasdbackend vgggg

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
-const app = require("../src/App");
-const connectDB = require("../src/config/db");
 
-// Mở kết nối Database mỗi khi có request tới Serverless API
-connectDB();
+// Vercel sẽ tự động tìm vào file này khi có request gọi '/api/...'
+const app = require("../backend/server");
 
-console.log("🚀 Serverless Backend đang hoạt động trên Vercel!");
-// Export app Express để Vercel tự động nhận diện
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,15 @@ const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
 
-// Khởi động server
-const startServer = async () => {
-  // Kết nối Database
-  await connectDB();
+// Mở kết nối Database
+connectDB();
 
-  // Lắng nghe port
+// Chỉ mở port khi chạy trên máy tính (local). Khi đẩy lên Vercel, nó sẽ tự bỏ qua.
+if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
   });
-};
+}
 
-startServer();
+// BẮT BUỘC: Export app ra để Vercel có thể nhận diện và khởi chạy Backend
+module.exports = app;
